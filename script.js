363209cd6a226d51886b285fd40cb6010fe24924
StefanoFoffanoVS <stefano.foffano@visualsoft.co.uk>
2024-10-30 11:26:29 +0100

    close the Nav on click

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function closeFullImage() {
     const modal = document.getElementById("fullImageModal");
     modal.style.display = "none";
 }
+
+
+//  Closes the menu on mobile when clicking a link 
+document.addEventListener('DOMContentLoaded', function () {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    const navbarToggler = document.querySelector('.navbar-toggler');
+
+    // Use event delegation on the navbar
+    navbarCollapse.addEventListener('click', (event) => {
+        if (event.target.matches('.nav-link')) {
+            // Close the navbar if it's open
+            if (navbarCollapse.classList.contains('show')) {
+                navbarToggler.click();
+            }
+        }
+    });
+});
